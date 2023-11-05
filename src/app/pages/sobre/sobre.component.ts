12502d15f8d21b66f6f5a36590bc41870e9f3cad
William Bigas Mauro <william.mauro@ibm.com>
2023-11-05 14:15:52 -0300

    Receber parâmetros de URL e Parâmetros Query

--- src/app/pages/sobre/sobre.component.ts
+++ src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit {
+
+  public id: number = 0;
+  public username: string = '';
+
+  public nome: string = '';
+  public sobrenome: string = '';
+
+  constructor(private activatedRoute: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+
+    // Parâmetros de URL
+    this.activatedRoute.params.subscribe(params => {
+      this.id = params['id'];
+      this.username = params['username'];
+      console.log(params)
+    });
+
+
+    // Query Params
+    this.activatedRoute.queryParams.subscribe(queryParams => {
+      this.nome = queryParams['nome'];
+      this.sobrenome = queryParams['sobrenome'];
+      console.log(queryParams)
+    });
+
+  }
+
 
 }
